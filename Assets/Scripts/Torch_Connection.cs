49ba1af53e72982845034f128a615c6a60bc1667
Evan Funnell <funnelle@mymacewan.ca>
2019-01-26 18:40:46 -0700

    Added torch logic

--- Assets/Scripts/Torch_Connection.cs
+++ Assets/Scripts/Torch_Connection.cs
@@ -3,50 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Torch_Connection : MonoBehaviour {
-    [SerializeField] private float detectionRange = 10f;
-
-    private SphereCollider torchDetectionArea;
-    private List<Collider> torches = new List<Collider>();
+    [SerializeField] private float lightIntensity = 15.44f;
 
     public bool powered = false;
+    public Light torchLight;
 
     // Start is called before the first frame update
     void Start() {
-        torchDetectionArea = GetComponent<SphereCollider>();
-        torchDetectionArea.radius = detectionRange;
+        torchLight.intensity = 0f;
     }
 
     // Update is called once per frame
     void Update() {
-        Debug.Log(torches.Count);
-        for (int i = 0; i < torches.Count; i++) {
-            //Debug.Log(torches[i]);
-            if(torches[i].GetComponent<Torch_Connection>().powered == true) {
-                powered = true;
-            }
-            else {
-                powered = false;
-            }
+        //controls light
+        if (powered) {
+            torchLight.intensity = lightIntensity;
         }
-        if (torches.Count == 0) {
-            powered = false;
+        else {
+            torchLight.intensity = 0f;
         }
-    }
 
-    private void OnTriggerEnter(Collider other) {
-        if (other.CompareTag("Torch")) {
-            if (!torches.Contains(other)) {
-                torches.Add(other);
-            }
-        }
     }
-
-    private void OnTriggerExit(Collider other) {
-        if (other.CompareTag("Torch")) {
-            if (torches.Contains(other)) {
-                torches.Remove(other);
-            }
-        }
-    }
-
 }
